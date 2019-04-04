21c901b3dfd15791ae423215b6e808f7894a63f9
Wyatt Whorton <bluebomber019@gmail.com>
2019-04-04 17:07:15 -0600

    Dolos Console INIT

---
+++ Dolos/Dolos Console Universal/Dolos Console Universal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dolos_Console_Universal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
